9c3028ca820297a57798f672de9c17136cb52996
Anton Revin <anton@revin.io>
2018-12-10 10:10:08 +0000

    Adding read + sort functionality to main.

--- main.cpp
+++ main.cpp
@@ -1,20 +1,25 @@
+#include <fstream>
+#include <iomanip>
 #include <iostream>
 #include <string>
 #include "Cleaner.h"
 
 using namespace std;
 
-void showUsage() {
-    cout << "Usage: ./a.out FILE [-a] [-f] [-o N]" << endl;
+void showError(string const description) {
+    cerr << "\033[1;31m" << description << "\033[0m" << endl;
+    cerr << "Usage: ./a.out FILE [-a] [-f] [-o N]" << endl;
 }
 
 int main(int argc, char** argv) {
     if (argc < 2) {
-        cout << "ERROR: No arguments given." << endl;
-        showUsage();
+        showError("ERROR: No arguments given.");
         return -1;
     }
 
+    /*
+        Retreive argument list
+    */
     string fileName{};
     string outputFormat{};
     int charLimit{};
@@ -26,13 +31,61 @@ int main(int argc, char** argv) {
         if ((outputFormat == "-o") && (argc = 4)) {
             charLimit = stoi(argv[3]);
         } else {
-            cout << "ERROR: Second argument missing or invalid." << endl;
-            showUsage();
+            showError("ERROR: Second argument missing or invalid.");
             return -1;
         }
     } catch (const std::exception& e) {
-        std::cerr << "ERROR: Incorrect argument list." << endl;
-        showUsage();
+        showError("ERROR: Incorrect argument list.");
         return -1;
+    }
+
+    /*
+        Open file and pipe to cleaner
+    */
+    ifstream fileStream{};
+    fileStream.open(fileName);
+    if (!fileStream.is_open()) {
+        showError("ERROR: Could not open the file.");
+        return -1;
+    }
+
+    Cleaner cleaner{};
+    string potentialWord{};
+
+    while (fileStream >> potentialWord) {
+        cleaner.addWord(potentialWord);
+    }
+
+    fileStream.close();
+
+    /*
+        Select output format
+    */
+    int maxWidth{cleaner.getLargestWordSize()};
+
+    switch (outputFormat[1]) {
+        case 'a':
+            auto comparator = [](pair<string, int> left, pair<string, int> right) {
+                return left.second >= right.second;
+            };
+            auto printer = [maxWidth](pair<string, int> currentPair) {
+                cout << left << setw(maxWidth) << currentPair.first << " " << currentPair.second << endl;
+            };
+            cleaner.sort(comparator);
+            cleaner.print(printer);
+            break;
+
+        case 'f':
+
+            break;
+
+        case 'o':
+
+            break;
+
+        default:
+            showError("ERROR: Incorrect argument list.");
+            return -1;
+            break;
     }
 }
